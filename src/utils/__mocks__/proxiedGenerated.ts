d165736b83bc83fa655b8bf7b4f31558079e62d4
song7788q <songpeis@gmail.com>
2022-08-10 17:53:42 +0800

    Change static files to CDN (#37)
    Change static files to CDN

---
+++ src/utils/__mocks__/proxiedGenerated.ts
@@ -0,0 +1,21 @@
+export let language = ["en", "zh"];
+export let removeDefaultStopWordFilter = false;
+export let removeDefaultStemmer = false;
+export const searchIndexUrl = "search-index.json?_=abc";
+export const searchResultLimits = 8;
+export const searchResultContextMaxLength = 50;
+export const explicitSearchResultPath = false;
+export const docsPluginIdForPreferredVersion = undefined;
+export const indexDocs = true;
+
+export function __setLanguage(value: string[]): void {
+  language = value;
+}
+
+export function __setRemoveDefaultStopWordFilter(value: boolean): void {
+  removeDefaultStopWordFilter = value;
+}
+
+export function __setRemoveDefaultStemmer(value: boolean): void {
+  removeDefaultStemmer = value;
+}
