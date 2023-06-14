09406f1b8077b1f4b9762cf19ec7f6fb662e2952
Jeffrey <color.dove@gmail.com>
2023-06-14 20:32:22 +0800

    (app) update wechat & slack link (#244)

--- src/theme/Footer/index.tsx
+++ src/theme/Footer/index.tsx
@@ -46,7 +46,7 @@ function Footer() {
                             </div>
                             <div className="social">
                                 <a
-                                    href="https://join.slack.com/t/apachedoriscommunity/shared_invite/zt-1h153f1ar-sTJB_QahY1SHvZdtPFoIOQ"
+                                    href="https://join.slack.com/t/apachedoriscommunity/shared_invite/zt-1x7x8fger-F7NoshFQn~djlvGdnEtxUQ"
                                     title="slack"
                                     className="item"
                                 >
