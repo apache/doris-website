2a9e7d523e494cca2898d423c7c97b0925f3784a
KassieZ <139741991+KassieZ@users.noreply.github.com>
2023-08-11 20:35:09 +0800

    Update newsletter - Best Practice and Tech Sharing  (#291)
    * Update newsletter.data.ts
    
    * Update newsletter.data.ts

--- src/constant/newsletter.data.ts
+++ src/constant/newsletter.data.ts
@@ -1,18 +1,18 @@
 export const NEWSLETTER_DATA = [
     {
+        tag: 'Best Practice',
+        title: 'For Entry-Level Data Engineers: How to Build a Simple but Solid Data Architecture',
+        to: '/blog/Poly',
+    },
+    {
         tag: 'Release Notes',
-        title: 'Apache Doris Announced the Official Release of Version 2.0.0 ',
+        title: 'Apache Doris Announced the Official Release of Version 2.0.0 ! ',
         to: '/docs/dev/releasenotes/release-2.0.0',
         hot: true,
     },
     {
-        tag: 'Best Practice',
-        title: 'Listen to That Poor BI Engineer: We Need Fast Join',
-        to: '/blog/Moka',
-    },
-    {
         tag: 'Tech Sharing',
-        title: 'Building a Log Analytics Solution 10 Times More Cost-Effective than Elasticsearch',
-        to: '/blog/Inverted%20Index/',
+        title: 'Is Your Latest Data Really the Latest? Check the Data Update Mechanism of Your Database',
+        to: '/blog/Data_Update',
     },
 ];
