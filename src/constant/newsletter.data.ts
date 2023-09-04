35c3511bdda3ea797f9c666801211ab42ee7476c
Jeffrey <color.dove@gmail.com>
2023-09-04 19:00:20 +0800

    (fix) fix 2.0.0 X64 download url error

--- src/constant/newsletter.data.ts
+++ src/constant/newsletter.data.ts
@@ -7,7 +7,7 @@ export const NEWSLETTER_DATA = [
     {
         tag: 'Release Notes',
         title: 'Apache Doris Announced the Official Release of Version 2.0.1 ! ',
-        to: 'https://github.com/apache/doris/issues/23640',
+        to: '/docs/dev/releasenotes/release-2.0.1',
         hot: true,
     },
     {
--- src/pages/download/index.tsx
+++ src/pages/download/index.tsx
@@ -285,7 +285,7 @@ export default function Download(): JSX.Element {
                                         {currentLocale === 'zh-CN' ? (
                                             <div className="notice-text">
                                                 详细升级注意事项请参考
-                                                <Link to="https://github.com/apache/doris/issues/23640">
+                                                <Link to="/docs/dev/releasenotes/release-2.0.1">
                                                     2.0.1 Release Note
                                                 </Link>
                                                 以及
@@ -303,7 +303,7 @@ export default function Download(): JSX.Element {
                                         ) : (
                                             <div className="notice-text">
                                                 For detailed upgrade precautions, please refer to the{' '}
-                                                <Link to="https://github.com/apache/doris/issues/23640">2.0.1</Link>
+                                                <Link to="/docs/dev/releasenotes/release-2.0.1">2.0.1</Link>
                                                 and the
                                                 <Link to="/docs/dev/install/standard-deployment">deployment</Link> and
                                                 cluster
