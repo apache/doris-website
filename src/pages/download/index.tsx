0654a768d35d502ae24b1a364af00c398ab850d9
Jeffrey <color.dove@gmail.com>
2023-09-04 17:29:24 +0800

    (typo) use 2.0.1 in download page (#302)

--- src/pages/download/index.tsx
+++ src/pages/download/index.tsx
@@ -303,7 +303,7 @@ export default function Download(): JSX.Element {
                                         ) : (
                                             <div className="notice-text">
                                                 For detailed upgrade precautions, please refer to the{' '}
-                                                <Link to="https://github.com/apache/doris/issues/22647">2.0.0</Link>
+                                                <Link to="https://github.com/apache/doris/issues/23640">2.0.1</Link>
                                                 and the
                                                 <Link to="/docs/dev/install/standard-deployment">deployment</Link> and
                                                 cluster
