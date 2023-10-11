32139dec9d50d929e42707412e568b3c905ba0fd
KassieZ <139741991+KassieZ@users.noreply.github.com>
2023-10-11 16:51:18 +0800

    Update newsletter.data.ts

--- src/constant/newsletter.data.ts
+++ src/constant/newsletter.data.ts
@@ -1,13 +1,14 @@
 export const NEWSLETTER_DATA = [
     {
-        tag: 'Best Practice',
-        title: 'LLM-Powered OLAP: the Tencent Experience with Apache Doris',
-        to: '/blog/Tencent-LLM',
-    },
-    {
         tag: 'Release Notes',
         title: 'Apache Doris Announced the Official Release of Version 2.0.2 ! ',
         to: 'https://github.com/apache/doris/issues/25011',
+        
+    },
+    {
+        tag: 'Top News',
+        title: 'Doris Summit Asia 2023 is Comming and Warmly Invite You to Join !',
+        to: 'https://doris-summit.org.cn/?utm_source=doris&utm_medium=website&utm_id=2023',
         hot: true,
     },
     {
