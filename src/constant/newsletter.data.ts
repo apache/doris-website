a37f7bdd6b47746fe78a6ce617929933160a5738
KassieZ <139741991+KassieZ@users.noreply.github.com>
2023-12-01 21:13:53 +0800

    Update newsletter.data.ts

--- src/constant/newsletter.data.ts
+++ src/constant/newsletter.data.ts
@@ -5,15 +5,14 @@ export const NEWSLETTER_DATA = [
         to: 'https://doris.apache.org/blog/release-2.0.2',
         
     },
+    {   tag: 'Best Practice',
+        title: `How Big Data Is Saving Lives in Real Time: IoV Data Analytics Helps Prevent Accidents`,
+        to: 'blog/how-big-data-is-saving-lives-in-real-time-iov-data-analytics-helps-prevent-accidents',
+        hot: true,
+    },
     {
         tag: 'Top News',
         title: `Apache Doris Summit Asia 2023: What Can You Expect From Apache Doris as a Data Warehouse?`,
         to: '/blog/apache-doris-summit-asia-2023-what-can-you-expect-from-apache-doris-as-a-data-warehouse',
-        hot: true,
-    },
-    {
-        tag: 'Best Practice',
-        title: 'Data Analysis for Live Streaming: What Happens in Real Time is Analyzed in Real Time',
-        to: '/blog/data-analysis-for-live-streaming-what-happens-in-real-time-is-analyzed-in-real-time',
-    },
+        },
 ];
