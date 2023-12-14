e3628955a974571d02e47ecbf32ba8f56676a2d4
KassieZ <139741991+KassieZ@users.noreply.github.com>
2023-12-14 17:26:39 +0800

    Update download.data.ts

--- src/constant/download.data.ts
+++ src/constant/download.data.ts
@@ -536,7 +536,7 @@ export function getAllRelease(locale: string) {
             ]
         },
         {
-            version: '1.2.7',
+            version: '1.2.7.1',
             date: '2023-09-04',
             note: '/docs/dev/releasenotes/release-1.2.7.1',
             download: [
@@ -678,7 +678,7 @@ export function getAllRelease(locale: string) {
 
 export enum VersionEnum {
     Latest = '2.0.3',
-    Prev = '1.2.7',
+    Prev = '1.2.7.1',
     Earlier = '1.1.5',
 }
 
