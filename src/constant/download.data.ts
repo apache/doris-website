853a41c6744a38b6055561e32db6b58c92adb5e6
KassieZ <139741991+KassieZ@users.noreply.github.com>
2023-12-14 17:32:16 +0800

    Update download.data.ts

--- src/constant/download.data.ts
+++ src/constant/download.data.ts
@@ -450,7 +450,7 @@ export function getAllRelease(locale: string) {
         {
             version: '2.0.3 ( Latest )',
             date: '2023-12-14',
-            note: 'https://github.com/apache/doris/issues/27909',
+            note: '/docs/releasenotes/release-2.0.3/',
             download: [
                 {
                     cpu: 'X64 ( avx2 )',
