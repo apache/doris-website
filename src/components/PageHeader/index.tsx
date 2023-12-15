a6856a8a8f7ea2bbf437407ab509a34ab4a77fbb
jeffrey <color.dove@gmail.com>
2023-12-15 14:29:35 +0800

    update user

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -3,12 +3,18 @@ import './style.scss';
 
 interface PageHeaderProps {
     title: string;
+    subtitle?: string;
+    extra?: any;
+    [any: string]: any;
 }
 
 export default function PageHeader(props: PageHeaderProps) {
+    const isBlog = props?.match?.path === '/blog';
     return (
-        <div className="page-header">
+        <div className={`page-header pb-[80px] ${isBlog ? 'white pt-[48px] ' : 'bg-[#F7F9FE] pt-[125px]'}`}>
             <h1 className="title">{props.title}</h1>
+            <div className="subtitle mt-6">{props.subtitle}</div>
+            {props?.extra && props?.extra}
         </div>
     );
 }
