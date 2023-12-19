176f372466954c8ea29f9de4159f4cf4cf0a608a
jeffrey <color.dove@gmail.com>
2023-12-19 21:34:27 +0800

    feat: update blog

---
+++ src/constant/generate-user.mjs
@@ -0,0 +1,18 @@
+import USERS_JSON from './users.json' assert { type: "json" };
+import fs from 'fs';
+function generateUser() {
+    const USERS = USERS_JSON.map((user, index) => {
+        const newUser = {...user};
+        // newUser.story = user['公司介绍'];
+        // newUser.name = user['英文名字'];
+        // newUser.category = user['行业分类'];
+        // newUser.category = user['行业分类'];
+        // newUser.logo = user['英文名字'];
+        newUser.order = index;
+        newUser.image = `/images/user-logo/${newUser.category}/${newUser.name}.jpg`;
+        return newUser;
+    });
+    fs.writeFileSync('./src/constant/users.data.json', JSON.stringify(USERS))
+}
+
+generateUser();
