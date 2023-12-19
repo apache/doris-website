02e393555c27c292e08a758d6f44575bc4eb0f9a
KassieZ <139741991+KassieZ@users.noreply.github.com>
2023-12-19 14:22:08 +0800

    [home](newsletter) Update Guance Blog on home page

--- src/constant/newsletter.data.ts
+++ src/constant/newsletter.data.ts
@@ -1,13 +1,13 @@
 export const NEWSLETTER_DATA = [
     {
-        tag: 'Best Practice',
-        title: 'Empowering cyber security by enabling 7 times faster log analysis',
-        to: 'blog/empowering-cyber-security-by-enabling-seven-times-faster-log-analysis',     
-    },
-    {   
         tag: 'Release Notes',
         title: 'Apache Doris 2.0.3 just released ',
         to: 'https://doris.apache.org/blog/release-note-2.0.3',
+    },
+    {   
+        tag: 'Best Practice',
+        title: 'From Elasticsearch to Apache Doris: upgrading an observability platform',
+        to: '/blog/from-elasticsearch-to-apache-doris-upgrading-an-observability-platform/',
         hot: true,
     },
     {
