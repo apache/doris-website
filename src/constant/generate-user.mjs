88d9f4c9190c0a90cc6a196bc2d0ff71510a8726
jeffrey <color.dove@gmail.com>
2023-12-20 11:04:27 +0800

    feat: update users

--- src/constant/generate-user.mjs
+++ src/constant/generate-user.mjs
@@ -5,10 +5,10 @@ function generateUser() {
     const USERS = USERS_JSON.map((user, index) => {
         const newUser = {};
         newUser.story = user['公司介绍'];
-        newUser.name = user['英文名字'];
+        newUser.name = user['英文名字'].trim();
         newUser.category = user['行业分类'];
         newUser.category = user['行业分类'];
-        newUser.logo = user['英文名字'];
+        newUser.logo = user['英文名字'].trim();
         newUser.order = index;
         newUser.to = user['博客链接'];
         newUser.image = `/images/user-logo/${newUser.category}/${newUser.name}.jpg`;
