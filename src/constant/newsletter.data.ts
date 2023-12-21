a36de471f1531d55c57278d17d934e27a3aabb54
KassieZ <139741991+KassieZ@users.noreply.github.com>
2023-12-21 17:08:46 +0800

    Update newsletter.data.ts

--- src/constant/newsletter.data.ts
+++ src/constant/newsletter.data.ts
@@ -1,7 +1,7 @@
 export const NEWSLETTER_DATA = [
     {
         tags: ['Top News'],
-        title: 'Apache Doris Summit Asia 2023: What Can You Expect From Apache Doris as a Data Warehouse?',
+        title: 'Apache Doris Summit Asia 2023: what can you expect from Apache Doris as a data warehouse?',
         content:
             'When it is cranberry and pumpkin season, we had the unforgettable Apache Doris Summit Asia 2023 with our remarkable committers, users, and community partners, to honor what we have achieved in the past year, and provide a preview of where we are going next.',
         to: '/blog/apache-doris-summit-asia-2023-what-can-you-expect-from-apache-doris-as-a-data-warehouse',
@@ -9,7 +9,7 @@ export const NEWSLETTER_DATA = [
     },
     {
         tags: ['Tech Sharing'],
-        title: 'Introduction to Apache Doris: A Next-Generation Real-Time Data Warehouse',
+        title: 'Introduction to Apache Doris: a next-generation real-time data warehouse',
         content:
             'This is a technical overview of Apache Doris, introducing how it enables fast query performance with its architectural design, features, and mechanisms.',
         to: '/blog/introduction-to-apache-doris-a-next-generation-real-time-data-warehouse',
@@ -17,14 +17,14 @@ export const NEWSLETTER_DATA = [
     },
     {
         tags: ['Tech Sharing'],
-        title: 'Log Analysis: Elasticsearch VS Apache Doris',
+        title: 'Log analysis: Elasticsearch vs Apache Doris',
         content: `As a major part of a company's data asset, logs brings values to businesses in three aspects: system observability, cyber security, and data analysis. They are your first resort for troubleshooting, your reference for improving system security, and your data mine where you can extract information that points to business growth.`,
         to: '/blog/log-analysis-elasticsearch-vs-apache-doris',
         image: 'newsletter-3.png',
     },
     {
         tags: ['Best Practice'],
-        title: 'LLM-Powered OLAP: the Tencent Experience with Apache Doris',
+        title: 'LLM-powered OLAP: the Tencent application with Apache Doris',
         content: `The exploration of a LLM+OLAP solution is a bumpy journey, but phew, it now works well for the Tencent case, and they're writing down every lesson learned to share with you.`,
         to: '/blog/Tencent-LLM',
         image: 'newsletter-4.jpeg',
