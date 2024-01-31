da4b9b10a39333b1d0b007ed0d61ae24c8a080c4
Wenlong Hu <962950733@qq.com>
2024-01-31 19:28:43 +0800

    (feat) update ecosystem page & users swiper

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,9 +5,7 @@ import { translate } from '@docusaurus/Translate';
 import './index.scss';
 import userCasesEn from '@site/userCases/en_US.json';
 import { Swiper, SwiperClass, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+
 import { Pagination } from 'swiper';
 import usePhone from '@site/src/hooks/use-phone';
 import PageHeader from '@site/src/components/PageHeader';
@@ -17,6 +15,7 @@ import UserItem from './user-item';
 import USERS from '../../constant/users.data.json';
 import ReadMore from '@site/src/components/ReadMore';
 import LinkWithArrow from '@site/src/components/link-arrow';
+import { UserSwiper } from './user-swiper';
 
 const ALL_TEXT = 'All';
 
@@ -42,24 +41,7 @@ export default function Users(): JSX.Element {
         setActive(ALL_TEXT);
     }, []);
 
-    const [swiperRef, setSwiperRef] = useState<SwiperClass>();
-
     const theSlides = useMemo(() => ['slide one', 'slide two'], []);
-
-    const handlePrevious = useCallback(() => {
-        swiperRef?.slidePrev();
-    }, [swiperRef]);
-
-    const handleNext = useCallback(() => {
-        swiperRef?.slideNext();
-    }, [swiperRef]);
-
-    const pagination = {
-        clickable: true,
-        renderBullet: function (index, className) {
-            return '<span class="' + className + '"></span>';
-        },
-    };
 
     function changeCategory(category: string) {
         setActive(category);
@@ -76,67 +58,6 @@ export default function Users(): JSX.Element {
         }
     }
 
-    function renderSwiper() {
-        const modules = [Pagination];
-        // if (!isPhone) {
-        //     modules.push(Navigation);
-        // }
-        return (
-            <div style={{ position: 'relative' }}>
-                {!isPhone && (
-                    <div
-                        onClick={handlePrevious}
-                        className="swiper-button-prev invisible  group-hover:visible"
-                        style={{ position: 'absolute', top: 'calc(50% - 2rem)', left: '-3rem', zIndex: 99 }}
-                    ></div>
-                )}
-
-                <Swiper
-                    pagination={pagination}
-                    spaceBetween={50}
-                    slidesPerView={1}
-                    navigation={false}
-                    modules={modules}
-                    loop={true}
-                    className="mySwiper"
-                    // style={{ minHeight: 480 }}
-                    onSlideChange={() => console.log('slide change')}
-                    onSwiper={setSwiperRef}
-                >
-                    {USER_STORIES.map(userStory => {
-                        return (
-                            <SwiperSlide key={userStory.title}>
-                                <div className="users-wall-list row flex flex-start pb-8 lg:pb-16">
-                                    <div>
-                                        <img
-                                            className="users-wall-img lg:w-[580px] lg:h-[248px]"
-                                            src={`${require(`@site/static/images/${userStory.image}`).default}`}
-                                            alt="users-wall-img"
-                                        />
-                                    </div>
-                                    <div className="w-[35.75rem] ml-4 lg:ml-12 flex flex-col py-4">
-                                        <h3 className="leading-[38px] text-2xl">{userStory.title}</h3>
-                                        <p className="my-6 text-base">
-                                            <strong className="font-normal">{userStory.author.name}</strong>
-                                            <span className="ml-6 text-[#4C576C]">{userStory.author.title}</span>
-                                        </p>
-                                        <ReadMore to={userStory.to} className="text-primary" />
-                                    </div>
-                                </div>
-                            </SwiperSlide>
-                        );
-                    })}
-                </Swiper>
-                {!isPhone && (
-                    <div
-                        onClick={handleNext}
-                        className="swiper-button-next invisible  group-hover:visible"
-                        style={{ position: 'absolute', top: 'calc(50% - 2rem)', right: '-3rem', zIndex: 99 }}
-                    ></div>
-                )}
-            </div>
-        );
-    }
     return (
         <Layout
             title={translate({ id: 'users.title', message: 'User Stories' })}
@@ -162,7 +83,7 @@ export default function Users(): JSX.Element {
             />
 
             <section className="group">
-                <div className="users-wall container lg:pt-[88px] ">{renderSwiper()}</div>
+                <div className="users-wall container lg:pt-[88px] ">{<UserSwiper />}</div>
             </section>
             <section className="lg:pt-[5.5rem] container pb-[88px]">
                 <div className="blog-list-wrap row mt-28 lg:mt-0">
