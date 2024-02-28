027cc786a0caf9a19118dd06e9caa5282b0b3adb
Kassiexii.Z <kzhou0504@gmail.com>
2024-02-28 10:57:59 +0800

    opt

--- src/constant/newsletter.data.ts
+++ src/constant/newsletter.data.ts
@@ -1,5 +1,12 @@
 export const NEWSLETTER_DATA = [
     {
+        tags: ['Release Notes'],
+        title: "Apache Doris 2.0.5 just released",
+        content: `Thanks to our community users and developers, about 217 improvements and bug fixes have been made in Doris 2.0.5 version.`,
+        to: '/blog/release-note-2.0.5',
+        image: '2.0.5.png',
+    },
+    {
         tags: ['Best Practice'],
         title: "A financial anti-fraud solution based on the Apache Doris data warehouse",
         content: `Financial fraud prevention is a race against time. This post will get into details about how a retail bank builds their fraud risk management platform based on Apache Doris and how it performs.`,
@@ -14,40 +21,11 @@ export const NEWSLETTER_DATA = [
         image: 'how-inverted-index-accelerates-text-searches-by-40-times.png',
     },
     {
-        tags: ['Release Notes'],
-        title: "Apache Doris 2.0.4 just released",
-        content: `Thanks to our community users and developers, 333 improvements and bug fixes have been made in Doris 2.0.4.`,
-        to: '/blog/release-note-2.0.4',
-        image: '2.0.4.png',
-    },
-    {
         tags: ['Best Practice'],
         title: "The financial sector's choice: fast, secure, and highly available real-time data warehousing based on Apache Doris",
         content: `A whole-journey guide for financial users looking for fast data processing performance, data security, and high service availability.`,
         to: '/blog/a-fast-secure-high-available-real-time-data-warehouse-based-on-apache-doris',
         image: 'apache-doris-a-fast-secure-and-highly-available-real-time-data-warehouse.png',
     },
-    // {
-    //     tags: ['Best Practice'],
-    //     title: 'Apache Doris speeds up data reporting, tagging, and data lake analytics',
-    //     content: `The user leverages the capabilities of Apache Doris in reporting, customer tagging, and data lake analytics and achieves high performance.`,
-    //     to: '/blog/apache-doris-speeds-up-data-reporting-tagging-and-data-lake-analytics',
-    //     image: 'apache-doris-speeds-up-data-reporting-data-lake-analytics.jpg',
-    // },
-    // {
-    //     tags: ['Top News'],
-    //     title: 'Apache Doris Summit Asia 2023: what can you expect from Apache Doris as a data warehouse?',
-    //     content:
-    //         'When it is cranberry and pumpkin season, we had the unforgettable Apache Doris Summit Asia 2023 with our remarkable committers, users, and community partners, to honor what we have achieved in the past year, and provide a preview of where we are going next.',
-    //     to: '/blog/apache-doris-summit-asia-2023-what-can-you-expect-from-apache-doris-as-a-data-warehouse',
-    //     image: 'newsletter-1.png',
-    // },
-    // {
-    //     tags: ['Tech Sharing'],
-    //     title: 'Introduction to Apache Doris: a next-generation real-time data warehouse',
-    //     content:
-    //         'This is a technical overview of Apache Doris, introducing how it enables fast query performance with its architectural design, features, and mechanisms.',
-    //     to: '/blog/introduction-to-apache-doris-a-next-generation-real-time-data-warehouse',
-    //     image: 'newsletter-2.png',
-    // }
+ 
 ];
