73edce7d3c65d7e1e1ddb372796f151b2a072ee7
jeffrey <color.dove@gmail.com>
2024-03-19 20:34:11 +0800

    [feat] update new features

--- src/theme/DocItemFooter/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="@docusaurus/plugin-content-docs" />
-/// <reference types="react" />
-import type { Props } from '@theme/DocItem';
-export default function DocItemFooter(props: Props): JSX.Element | null;
