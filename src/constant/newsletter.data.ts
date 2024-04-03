79b896d5671110bd508e199aaba76a89b3dadf53
KassieZ <139741991+KassieZ@users.noreply.github.com>
2024-04-03 17:59:31 +0800

    opt

--- src/constant/newsletter.data.ts
+++ src/constant/newsletter.data.ts
@@ -2,7 +2,7 @@ export const NEWSLETTER_DATA = [
     {
         tags: ['Release Notes'],
         title: "Apache Doris 2.1.1 just released",
-        content: `We appreciate the 237 contributors who made nearly 6000 commits in total to the Apache Doris project, and the nearly 100 enterprise users who provided valuable feedback.`,
+        content: `Dear community, Apache Doris 2.1.1 is now available, with several enhancements and bug fixes based on 2.1.0, enabling smoother user experience.`,
         to: '/blog/release-note-2.1.1',
         image: '2.1.1.png',
     },
