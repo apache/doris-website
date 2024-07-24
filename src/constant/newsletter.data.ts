26a189741e7b7fe055e6a3c18e454de492e161b9
KassieZ <139741991+KassieZ@users.noreply.github.com>
2024-07-24 20:05:03 +0800

    [download](2.1.5) Update 2.1.5 release and download link (#912)

--- src/constant/newsletter.data.ts
+++ src/constant/newsletter.data.ts
@@ -1,5 +1,11 @@
 export const NEWSLETTER_DATA = [
-
+    {
+        tags: ['Release Note'],
+        title: "Apache Doris version 2.1.5 has been released",
+        content: `In this update, we have optimized various functional experiences for data lakehouse and high concurrency scenarios, functionalities of asynchronous materialized views. Additionaly, we have implemented several improvemnents and bug fixes to enhance the stability.`,
+        to: '/blog/release-note-2.1.5',
+        image: '2.1.5.jpg',
+    },
     {
         tags: ['Best Practice'],
         title: "Migrate data lakehouse from BigQuery to Apache Doris, saving $4,500 per month ",
@@ -7,14 +13,6 @@ export const NEWSLETTER_DATA = [
         to: '/blog/migrate-lakehouse-from-bigquery-to-doris',
         image: 'migrate-lakehouse-from-bigquery-to-apache-doris.jpg',
     },
-    {
-        tags: ['Release Note'],
-        title: "Apache Doris version 2.0.13 has been released",
-        content: `Thanks to our community users and developers, about 112 improvements and bug fixes have been made in Doris 2.0.13 version`,
-        to: '/blog/release-note-2.0.13',
-        image: '2.0.13.jpg',
-    },
-
     {
         tags: ['Top News'],
         title: "Why Apache Doris is the Best Open Source Alternative to Rockset",
