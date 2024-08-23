4cf739b7d9e1d0942553ce8a969c8165febf6b6a
KassieZ <139741991+KassieZ@users.noreply.github.com>
2024-08-23 16:48:34 +0800

    1

--- src/constant/newsletter.data.ts
+++ src/constant/newsletter.data.ts
@@ -1,18 +1,19 @@
 export const NEWSLETTER_DATA = [
     {
+        tags: ['Release Note'],
+        title: "Apache Doris version 3.0.1 just released",
+        content: `In this version, Apache Doris has improvements in compute-storage decoupling, lakehouse, semi-structured data analysis and more.`,
+        to: '/blog/release-note-3.0.1',
+        image: '3.0.1.jpg',
+    },
+    {
         tags: ['Tech Sharing'],
         title: "Automatic and flexible data sharding: Auto Partition in Apache Doris",
         content: `Apache Doris 2.1.0 introduces Auto Partition. It supports partitioning data by RANGE or by LIST and further enhances flexibility on top of automatic partitioning.`,
         to: '/blog/auto-partition-in-apache-doris',
         image: 'auto-partition-in-apache-doris.jpg',
     },
-    {
-        tags: ['Release Note'],
-        title: "Apache Doris version 2.0.14 has been released",
-        content: `Thanks to our community users and developers, about 110 improvements and bug fixes have been made in Doris 2.0.14 version`,
-        to: '/blog/release-note-2.0.14',
-        image: '2.0.14.jpg',
-    },
+
     {
         tags: ['Best Practice'],
         title: "Migrate data lakehouse from BigQuery to Apache Doris, saving $4,500 per month ",
