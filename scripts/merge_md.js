834dac1fb90318fcfe27f4a587b8632663219f4c
0xFlackoLFG <k9745upup@gmail.com>
2024-08-23 21:52:51 +0800

    license header

--- scripts/merge_md.js
+++ scripts/merge_md.js
@@ -1,8 +1,25 @@
+//  Licensed to the Apache Software Foundation (ASF) under one
+//  or more contributor license agreements.  See the NOTICE file
+//  distributed with this work for additional information
+//  regarding copyright ownership.  The ASF licenses this file
+//  to you under the Apache License, Version 2.0 (the
+//  "License"); you may not use this file except in compliance
+//  with the License.  You may obtain a copy of the License at
+
+//    http://www.apache.org/licenses/LICENSE-2.0
+
+//  Unless required by applicable law or agreed to in writing,
+//  software distributed under the License is distributed on an
+//  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+//  KIND, either express or implied.  See the License for the
+//  specific language governing permissions and limitations
+//  under the License.
+
 const fs = require('fs');
 const path = require('path');
 
-const sidebarPath = 'versioned_sidebars/version-2.1-cn-sidebars.json'
-const docsBaseDir = 'i18n/zh-CN/docusaurus-plugin-content-docs/version-2.1'
+const sidebarPath = 'versioned_sidebars/version-3.0-sidebars.json'
+const docsBaseDir = 'i18n/zh-CN/docusaurus-plugin-content-docs/version-3.0'
 const outputPath = 'doc.md'
 
 const fileLinkName = {};
@@ -28,10 +45,7 @@ function replaceLinkWrap(chapter) {
         if (link.startsWith('http')) {
             return match;
         } else if (/\.(png|jpeg|svg|gif|jpg)$/.test(link)) {
-            console.log(link,'link')
             const imgLink = link.replace(/images\//, 'static/images/');
-            console.log(linkName,'linkName')
-            console.log(imgLink,'imgLink')
             return `[${linkName}](${imgLink})`;
         } else {
             if (link.includes('.md#') && frag) {
