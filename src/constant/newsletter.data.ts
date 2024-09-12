9f0fb153cac28c994601d7ef502cf24e021e7caa
KassieZ <139741991+KassieZ@users.noreply.github.com>
2024-09-12 20:07:57 +0800

    1

--- src/constant/newsletter.data.ts
+++ src/constant/newsletter.data.ts
@@ -7,6 +7,13 @@ export const NEWSLETTER_DATA = [
         image: '2.1.6.jpg',
     },
     {
+        tags: ['Best Practice'],
+        title: "Real-time data warehouse in TikTok based on Apache Doris",
+        content: `We are excited to invite the data platform team of TikTok to talk about how they use Apache Doris in their real-time data architecture and how they benefit from it, which could serve as a model for effective real-time data warehousing.`,
+        to: '/blog/real-time-data-warehouse-in-TikTok-based-on-doris',
+        image: 'real-time-data-warehouse-in-tiktok-based-on-apache-doris.png',
+    },
+    {
         tags: ['Top News'],
         title: "How AI unicorn MiniMax migrated from Loki and built a PB-scale logging system with Apache Doris",
         content: `Serving a PB-scale data size with over 99.9% availability, Apache Doris is the vital signs monitor of MiniMax, a generative AI startup backed by Alibaba.`,
@@ -19,14 +26,6 @@ export const NEWSLETTER_DATA = [
         content: `Apache Doris 2.1.0 introduces Auto Partition. It supports partitioning data by RANGE or by LIST and further enhances flexibility on top of automatic partitioning.`,
         to: '/blog/auto-partition-in-apache-doris',
         image: 'auto-partition-in-apache-doris.jpg',
-    },
-
-    {
-        tags: ['Best Practice'],
-        title: "Migrate data lakehouse from BigQuery to Apache Doris, saving $4,500 per month ",
-        content: `Dien Tran Thanh shares his firsthand experience, proven best practices, and insightful suggestions for migrating from BigQuery to Apache Doris to build a more cost-efficient data warehouse.`,
-        to: '/blog/migrate-lakehouse-from-bigquery-to-doris',
-        image: 'migrate-lakehouse-from-bigquery-to-apache-doris.jpg',
     },
 
 
