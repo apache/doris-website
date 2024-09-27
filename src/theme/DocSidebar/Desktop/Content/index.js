240c0e7ce99a2a080650e2c2e4f4427ef97155de
Jeffrey <color.dove@gmail.com>
2024-09-27 12:41:46 +0900

    fix: fix the 3.0/2.1 version docs edit this page link error (#1146)
    ![image](https://github.com/user-attachments/assets/f3a66f56-0b5d-4f45-adc1-b6a1e2be6da8)

--- src/theme/DocSidebar/Desktop/Content/index.js
+++ src/theme/DocSidebar/Desktop/Content/index.js
@@ -8,6 +8,7 @@ import DownloadPdfActive from '@site/static/images/download-pdf-active.svg';
 import DocSidebarItems from '@theme/DocSidebarItems';
 import styles from './styles.module.css';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import { DOWNLOAD_PDFS } from '@site/src/constant/download.data';
 // import { getLatestVersion } from '../../../../../docusaurus.config';
 // import { useActivePluginAndVersion } from '@docusaurus/plugin-content-docs/client';
 // import { useGlobalData } from '@docusaurus/useGlobalData';
@@ -24,6 +25,20 @@ function useShowAnnouncementBar() {
         [isActive],
     );
     return isActive && showAnnouncementBar;
+}
+
+function downloadFile(url, filename) {
+    var xml = new XMLHttpRequest();
+    xml.open('GET', url, true);
+    xml.responseType = 'blob';
+    xml.onload = function () {
+        var url = window.URL.createObjectURL(xml.response);
+        var a = document.createElement('a');
+        a.href = url;
+        a.download = filename;
+        a.click();
+    };
+    xml.send();
 }
 
 export default function DocSidebarDesktopContent({ path, sidebar, className }) {
@@ -68,9 +83,16 @@ export default function DocSidebarDesktopContent({ path, sidebar, className }) {
             <ul className={clsx(ThemeClassNames.docs.docSidebarMenu, 'menu__list')}>
                 {showVersion && <div className={styles.currentVersion}>
                     {isEN ? 'Version:' : '当前版本:'} {currentVersion}
-                    {/* <div onMouseLeave={() => setIshover(false)} onMouseEnter={() => setIshover(true)} className="cursor-pointer">
-                        {isHover ? <DownloadPdfActive /> : <DownloadPdf />}
-                    </div> */}
+                    {
+                        ['3.0', '2.0', '2.1'].includes(currentVersion) && (
+                            <div onMouseLeave={() => setIshover(false)} onMouseEnter={() => setIshover(true)} className="cursor-pointer" onClick={() => {
+                                const pdfInfo = DOWNLOAD_PDFS.find(item => item.version === currentVersion);
+                                downloadFile(pdfInfo.link, pdfInfo.filename);
+                            }}>
+                                {isHover ? <DownloadPdfActive /> : <DownloadPdf />}
+                            </div>
+                        )
+                    }
                 </div>}
                 <DocSidebarItems items={sidebar} activePath={path} level={1} />
             </ul>
