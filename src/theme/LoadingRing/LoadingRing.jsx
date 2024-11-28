2daa525066aaec6ace24f021f5b3b148e75c4074
yangon <2689991790@qq.com>
2024-11-28 15:49:27 +0800

    rebase master (#1416)
    Signed-off-by: zenghua <441651826@qq.com>
    Co-authored-by: wudi <wudi@selectdb.com>
    Co-authored-by: xzj7019 <xiongzhongjian@selectdb.com>
    Co-authored-by: Pxl <pxl290@qq.com>
    Co-authored-by: Mingyu Chen (Rayner) <morningman@163.com>
    Co-authored-by: wangbo <wangbo@apache.org>
    Co-authored-by: smiletan <intelligentfu@163.com>
    Co-authored-by: Kaijie Chen <ckj@apache.org>
    Co-authored-by: zhangstar333 <2561612514@qq.com>
    Co-authored-by: KassieZ <139741991+KassieZ@users.noreply.github.com>
    Co-authored-by: wangtianyi2004 <376612600@qq.com>
    Co-authored-by: Mingxi <71588583+joker-star-l@users.noreply.github.com>
    Co-authored-by: Ruffian Jiang <jiangyj0516@outlook.com>
    Co-authored-by: zclllhhjj <zhaochangle@selectdb.com>
    Co-authored-by: 小可耐 <46134044+sdhzwc@users.noreply.github.com>
    Co-authored-by: Ceng <441651826@qq.com>
    Co-authored-by: lihangyu <15605149486@163.com>
    Co-authored-by: zhangstar333 <zhangsida@selectdb.com>
    Co-authored-by: hui lai <1353307710@qq.com>
    Co-authored-by: liyang <liyang@selectdb.com>
    Co-authored-by: Jeffrey <color.dove@gmail.com>
    Co-authored-by: catpineapple <1391869588@qq.com>
    Co-authored-by: walter <w41ter.l@gmail.com>
    Co-authored-by: Xujian Duan <50550370+DarvenDuan@users.noreply.github.com>
    Co-authored-by: Vallish Pai <vallishpai@gmail.com>
    Co-authored-by: LiBinfeng <1204975323@qq.com>

--- src/theme/LoadingRing/LoadingRing.jsx
+++ src/theme/LoadingRing/LoadingRing.jsx
@@ -2,18 +2,11 @@
 import React from "react";
 import clsx from "clsx";
 import styles from "./LoadingRing.module.css";
-
-export default function LoadingRing({
-  className,
-}: {
-  className?: string;
-}): React.ReactElement {
-  return (
-    <div className={clsx(styles.loadingRing, className)}>
+export default function LoadingRing({ className, }) {
+    return (<div className={clsx(styles.loadingRing, className)}>
       <div></div>
       <div></div>
       <div></div>
       <div></div>
-    </div>
-  );
+    </div>);
 }
