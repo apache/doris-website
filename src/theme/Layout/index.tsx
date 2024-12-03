5b56730a1e9d37835a1bb46171ce532710619c38
yangon <2689991790@qq.com>
2024-12-03 17:08:37 +0800

    Modify article style (#1443)
    Co-authored-by: liyang <liyang@selectdb.com>

--- src/theme/Layout/index.tsx
+++ src/theme/Layout/index.tsx
@@ -1,56 +1,56 @@
 import React, { useEffect } from 'react';
 import clsx from 'clsx';
 import ErrorBoundary from '@docusaurus/ErrorBoundary';
-import {
-  PageMetadata,
-  SkipToContentFallbackId,
-  ThemeClassNames,
-} from '@docusaurus/theme-common';
-import {useKeyboardNavigation} from '@docusaurus/theme-common/internal';
+import { PageMetadata, SkipToContentFallbackId, ThemeClassNames } from '@docusaurus/theme-common';
+import { useKeyboardNavigation } from '@docusaurus/theme-common/internal';
 import SkipToContent from '@theme/SkipToContent';
 import AnnouncementBar from '@theme/AnnouncementBar';
 import Navbar from '@theme/Navbar';
 import Footer from '@theme/Footer';
 import LayoutProvider from '@theme/Layout/Provider';
 import ErrorPageContent from '@theme/ErrorPageContent';
-import type {Props} from '@theme/Layout';
+import type { Props } from '@theme/Layout';
 import styles from './styles.module.css';
+import { useHistory } from '@docusaurus/router';
 
 export default function Layout(props: Props): JSX.Element {
-  const {
-    children,
-    noFooter,
-    wrapperClassName,
-    // Not really layout-related, but kept for convenience/retro-compatibility
-    title,
-    description,
-  } = props;
+    const {
+        children,
+        noFooter,
+        wrapperClassName,
+        // Not really layout-related, but kept for convenience/retro-compatibility
+        title,
+        description,
+    } = props;
+    const history = useHistory();
+    useKeyboardNavigation();
 
-  useKeyboardNavigation();
+    useEffect(() => {
+        if (
+            history.location.pathname?.length > 1 &&
+            history.location.pathname[history.location.pathname.length - 1] === '/'
+        )
+            history.replace(history.location.pathname.slice(0, -1));
+    }, []);
 
-  return (
-    <LayoutProvider>
-      <PageMetadata title={title} description={description} />
+    return (
+        <LayoutProvider>
+            <PageMetadata title={title} description={description} />
 
-      <SkipToContent />
+            <SkipToContent />
 
-      <AnnouncementBar />
+            <AnnouncementBar />
 
-      <Navbar />
+            <Navbar />
 
-      <div
-        id={SkipToContentFallbackId}
-        className={clsx(
-          ThemeClassNames.wrapper.main,
-          styles.mainWrapper,
-          wrapperClassName,
-        )}>
-        <ErrorBoundary fallback={(params) => <ErrorPageContent {...params} />}>
-          {children}
-        </ErrorBoundary>
-      </div>
+            <div
+                id={SkipToContentFallbackId}
+                className={clsx(ThemeClassNames.wrapper.main, styles.mainWrapper, wrapperClassName)}
+            >
+                <ErrorBoundary fallback={params => <ErrorPageContent {...params} />}>{children}</ErrorBoundary>
+            </div>
 
-      {!noFooter && <Footer />}
-    </LayoutProvider>
-  );
+            {!noFooter && <Footer />}
+        </LayoutProvider>
+    );
 }
