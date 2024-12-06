bcdd5c964b770a9f4ec7cd6ba1734667c2650061
yangon <2689991790@qq.com>
2024-12-06 11:32:14 +0800

    fix: fix the searchbar version (#1467)
    Co-authored-by: liyang <liyang@selectdb.com>

--- src/theme/SearchBar/SearchBar.jsx
+++ src/theme/SearchBar/SearchBar.jsx
@@ -1,30 +1,25 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import clsx from 'clsx';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useIsBrowser from '@docusaurus/useIsBrowser';
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 import { useHistory, useLocation } from '@docusaurus/router';
 import { translate } from '@docusaurus/Translate';
 import { ReactContextError, useDocsPreferredVersion } from '@docusaurus/theme-common';
 import { useActivePlugin } from '@docusaurus/plugin-content-docs/client';
-import { fetchIndexesByWorker, searchByWorker } from './searchByWorker';
+import { fetchIndexes } from './fetchIndexes';
+import { SearchSourceFactory } from '../../utils/SearchSourceFactory';
 import { SuggestionTemplate } from './SuggestionTemplate';
 import { EmptyTemplate } from './EmptyTemplate';
 import {
+    searchResultLimits,
     Mark,
     searchBarShortcut,
     searchBarShortcutHint,
-    searchBarPosition,
     docsPluginIdForPreferredVersion,
     indexDocs,
-    searchContextByPaths,
-    hideSearchBarWithNoSearchContext,
-    useAllContextsWithNoSearchContext,
 } from '../../utils/proxiedGenerated';
 import LoadingRing from '../LoadingRing/LoadingRing';
 import styles from './SearchBar.module.css';
-import { normalizeContextByPath } from '../../utils/normalizeContextByPath';
-import useIsDocPage from '@site/src/hooks/use-is-doc';
-import { VERSIONS, DEFAULT_VERSION } from '@site/src/constant/common';
 async function fetchAutoCompleteJS() {
     const autoCompleteModule = await import('@easyops-cn/autocomplete.js');
     const autoComplete = autoCompleteModule.default;
@@ -39,171 +34,55 @@ async function fetchAutoCompleteJS() {
 }
 const SEARCH_PARAM_HIGHLIGHT = '_highlight';
 export default function SearchBar({ handleSearchBarToggle }) {
-    const isBrowser = useIsBrowser();
-    const location = useLocation();
-    // const curVersion = useRef(DEFAULT_VERSION);
-    const [curVersion, setCurVersion] = useState(DEFAULT_VERSION);
     const {
         siteConfig: { baseUrl },
-        i18n: { currentLocale },
     } = useDocusaurusContext();
     // It returns undefined for non-docs pages
     const activePlugin = useActivePlugin();
-    const [isDocsPage] = useIsDocPage(false);
     let versionUrl = baseUrl;
-    if (location?.pathname && location.pathname.includes('zh-CN') && !versionUrl.includes('zh-CN')) {
-        versionUrl = baseUrl + 'zh-CN/';
-    }
-    if (location?.pathname) {
-        VERSIONS.forEach(version => {
-            if (location.pathname.includes(version)) {
-                versionUrl += `docs/${version}/`;
-            }
-        });
-    }
-
     // For non-docs pages while using plugin-content-docs with custom ids,
     // this will throw an error of:
     //   > Docusaurus plugin global data not found for "docusaurus-plugin-content-docs" plugin with id "default".
     // It seems that we can not get the correct id for non-docs pages.
-    // try {
-    //     // The try-catch is a hack because useDocsPreferredVersion just throws an
-    //     // exception when versions are not used.
-    //     // The same hack is used in SearchPage.tsx
-    //     // eslint-disable-next-line react-hooks/rules-of-hooks
-    //     const { preferredVersion } = useDocsPreferredVersion(activePlugin?.pluginId ?? docsPluginIdForPreferredVersion);
-    //     console.log('preferredVersion',preferredVersion);
-
-    //     if (preferredVersion && !preferredVersion.isLast) {
-    //         versionUrl = preferredVersion.path + "/";
-    //     }
-    // }
-    // catch (e) {
-    //     if (indexDocs) {
-    //         if (e instanceof ReactContextError) {
-    //             /* ignore, happens when website doesn't use versions */
-    //         }
-    //         else {
-    //             throw e;
-    //         }
-    //     }
-    // }
-
+    try {
+        // The try-catch is a hack because useDocsPreferredVersion just throws an
+        // exception when versions are not used.
+        // The same hack is used in SearchPage.tsx
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        const { preferredVersion } = useDocsPreferredVersion(activePlugin?.pluginId ?? docsPluginIdForPreferredVersion);
+        if (preferredVersion && !preferredVersion.isLast) {
+            versionUrl = preferredVersion.path + '/';
+        }
+    } catch (e) {
+        if (indexDocs) {
+            if (e instanceof ReactContextError) {
+                /* ignore, happens when website doesn't use versions */
+            } else {
+                throw e;
+            }
+        }
+    }
     const history = useHistory();
+    const location = useLocation();
     const searchBarRef = useRef(null);
-    const indexStateMap = useRef(new Map());
+    const indexState = useRef('empty'); // empty, loaded, done
     // Should the input be focused after the index is loaded?
     const focusAfterIndexLoaded = useRef(false);
     const [loading, setLoading] = useState(false);
     const [inputChanged, setInputChanged] = useState(false);
     const [inputValue, setInputValue] = useState('');
     const search = useRef(null);
-    const prevSearchContext = useRef('');
-    const [searchContext, setSearchContext] = useState('');
-    useEffect(() => {
-        if (!Array.isArray(searchContextByPaths)) {
-            return;
-        }
-        let nextSearchContext = '';
-        if (location.pathname.startsWith(versionUrl)) {
-            const uri = location.pathname.substring(versionUrl.length);
-            let matchedPath;
-            for (const _path of searchContextByPaths) {
-                const path = typeof _path === 'string' ? _path : _path.path;
-                if (uri === path || uri.startsWith(`${path}/`)) {
-                    matchedPath = path;
-                    break;
-                }
-            }
-            if (matchedPath) {
-                nextSearchContext = matchedPath;
-            }
-        }
-        if (prevSearchContext.current !== nextSearchContext) {
-            // Reset index state map once search context is changed.
-            indexStateMap.current.delete(nextSearchContext);
-            prevSearchContext.current = nextSearchContext;
-        }
-        setSearchContext(nextSearchContext);
-    }, [location.pathname, versionUrl]);
-    const hidden = !!hideSearchBarWithNoSearchContext && Array.isArray(searchContextByPaths) && searchContext === '';
-    const loadIndex = useCallback(async (forceLoad = false) => {
-        if ((hidden || indexStateMap.current.get(searchContext)) && !forceLoad) {
+    const loadIndex = useCallback(async () => {
+        if (indexState.current !== 'empty') {
             // Do not load the index (again) if its already loaded or in the process of being loaded.
             return;
         }
-        indexStateMap.current.set(searchContext, 'loading');
-        search.current?.autocomplete.destroy();
+        indexState.current = 'loading';
         setLoading(true);
-        const [autoComplete] = await Promise.all([
+        const [{ wrappedIndexes, zhDictionary }, autoComplete] = await Promise.all([
+            fetchIndexes(versionUrl),
             fetchAutoCompleteJS(),
-            fetchIndexesByWorker(versionUrl, searchContext),
         ]);
-        const searchFooterLinkElement = ({ query, isEmpty }) => {
-            const a = document.createElement('a');
-            const params = new URLSearchParams();
-            params.set('q', query);
-            let linkText;
-            if (searchContext) {
-                const detailedSearchContext =
-                    searchContext && Array.isArray(searchContextByPaths)
-                        ? searchContextByPaths.find(item =>
-                              typeof item === 'string' ? item === searchContext : item.path === searchContext,
-                          )
-                        : searchContext;
-                const translatedSearchContext = detailedSearchContext
-                    ? normalizeContextByPath(detailedSearchContext, currentLocale).label
-                    : searchContext;
-                if (useAllContextsWithNoSearchContext && isEmpty) {
-                    linkText = translate(
-                        {
-                            id: 'theme.SearchBar.seeAllOutsideContext',
-                            message: 'See all results outside "{context}"',
-                        },
-                        { context: translatedSearchContext },
-                    );
-                } else {
-                    linkText = translate(
-                        {
-                            id: 'theme.SearchBar.searchInContext',
-                            message: 'See all results within "{context}"',
-                        },
-                        { context: translatedSearchContext },
-                    );
-                }
-            } else {
-                linkText = translate({
-                    id: 'theme.SearchBar.seeAll',
-                    message: 'See all results',
-                });
-            }
-            if (
-                searchContext &&
-                Array.isArray(searchContextByPaths) &&
-                (!useAllContextsWithNoSearchContext || !isEmpty)
-            ) {
-                params.set('ctx', searchContext);
-            }
-            if (versionUrl !== baseUrl) {
-                if (!versionUrl.startsWith(baseUrl)) {
-                    throw new Error(
-                        `Version url '${versionUrl}' does not start with base url '${baseUrl}', this is a bug of \`@easyops-cn/docusaurus-search-local\`, please report it.`,
-                    );
-                }
-                params.set('version', versionUrl.substring(baseUrl.length));
-            }
-            const url = `${baseUrl}search/?${params.toString()}`;
-            a.href = url;
-            a.textContent = linkText;
-            a.addEventListener('click', e => {
-                if (!e.ctrlKey && !e.metaKey) {
-                    e.preventDefault();
-                    search.current?.autocomplete.close();
-                    history.push(url);
-                }
-            });
-            return a;
-        };
         search.current = autoComplete(
             searchBarRef.current,
             {
@@ -211,9 +90,7 @@ export default function SearchBar({ handleSearchBarToggle }) {
                 autoselect: true,
                 openOnFocus: true,
                 cssClasses: {
-                    root: clsx(styles.searchBar, {
-                        [styles.searchBarLeft]: searchBarPosition === 'left',
-                    }),
+                    root: styles.searchBar,
                     noPrefix: true,
                     dropdownMenu: styles.dropdownMenu,
                     input: styles.input,
@@ -227,18 +104,28 @@ export default function SearchBar({ handleSearchBarToggle }) {
             },
             [
                 {
-                    source: async (input, callback) => {
-                        const result = await searchByWorker(versionUrl, searchContext, input);
-                        callback(result);
-                    },
+                    source: SearchSourceFactory(wrappedIndexes, zhDictionary, searchResultLimits),
                     templates: {
                         suggestion: SuggestionTemplate,
                         empty: EmptyTemplate,
                         footer: ({ query, isEmpty }) => {
-                            if (isEmpty && (!searchContext || !useAllContextsWithNoSearchContext)) {
+                            if (isEmpty) {
                                 return;
                             }
-                            const a = searchFooterLinkElement({ query, isEmpty });
+                            const a = document.createElement('a');
+                            const url = `${baseUrl}search?q=${encodeURIComponent(query)}`;
+                            a.href = url;
+                            a.textContent = translate({
+                                id: 'theme.SearchBar.seeAll',
+                                message: 'See all results',
+                            });
+                            a.addEventListener('click', e => {
+                                if (!e.ctrlKey && !e.metaKey) {
+                                    e.preventDefault();
+                                    search.current.autocomplete.close();
+                                    history.push(url);
+                                }
+                            });
                             const div = document.createElement('div');
                             div.className = styles.hitFooter;
                             div.appendChild(a);
@@ -266,21 +153,23 @@ export default function SearchBar({ handleSearchBarToggle }) {
             .on('autocomplete:closed', () => {
                 searchBarRef.current?.blur();
             });
-        indexStateMap.current.set(searchContext, 'done');
+        indexState.current = 'done';
         setLoading(false);
         if (focusAfterIndexLoaded.current) {
             const input = searchBarRef.current;
             if (input.value) {
-                search.current?.autocomplete.open();
+                search.current.autocomplete.open();
             }
             input.focus();
         }
-    }, [hidden, searchContext, versionUrl, baseUrl, history]);
+    }, [baseUrl, versionUrl, history]);
     useEffect(() => {
         if (!Mark) {
             return;
         }
-        const keywords = isBrowser ? new URLSearchParams(location.search).getAll(SEARCH_PARAM_HIGHLIGHT) : [];
+        const keywords = ExecutionEnvironment.canUseDOM
+            ? new URLSearchParams(location.search).getAll(SEARCH_PARAM_HIGHLIGHT)
+            : [];
         // A workaround to fix an issue of highlighting in code blocks.
         // See https://github.com/easyops-cn/docusaurus-search-local/issues/92
         // Code blocks will be re-rendered after this `useEffect` ran.
@@ -299,7 +188,7 @@ export default function SearchBar({ handleSearchBarToggle }) {
             setInputValue(keywords.join(' '));
             search.current?.autocomplete.setVal(keywords.join(' '));
         });
-    }, [isBrowser, location.search, location.pathname]);
+    }, [location.search, location.pathname]);
     const [focused, setFocused] = useState(false);
     const onInputFocus = useCallback(() => {
         focusAfterIndexLoaded.current = true;
@@ -321,14 +210,16 @@ export default function SearchBar({ handleSearchBarToggle }) {
         }
     }, []);
     // Implement hint icons for the search shortcuts on mac and the rest operating systems.
-    const isMac = isBrowser ? /mac/i.test(navigator.userAgentData?.platform ?? navigator.platform) : false;
+    const isMac = ExecutionEnvironment.canUseDOM
+        ? /mac/i.test(navigator.userAgentData?.platform ?? navigator.platform)
+        : false;
     useEffect(() => {
         if (!searchBarShortcut) {
             return;
         }
         // Add shortcuts command/ctrl + K
         const handleShortcut = event => {
-            if ((isMac ? event.metaKey : event.ctrlKey) && (event.key === 'k' || event.key === 'K')) {
+            if ((isMac ? event.metaKey : event.ctrlKey) && event.code === 'KeyK') {
                 event.preventDefault();
                 searchBarRef.current?.focus();
                 onInputFocus();
@@ -339,24 +230,6 @@ export default function SearchBar({ handleSearchBarToggle }) {
             document.removeEventListener('keydown', handleShortcut);
         };
     }, [isMac, onInputFocus]);
-
-    useEffect(() => {
-        const pathHaveVer = VERSIONS.some(item => location.pathname.includes(item));
-        if (!pathHaveVer && curVersion !== '2.1') {
-            setCurVersion('2.1');
-        } else {
-            VERSIONS.forEach(item => {
-                if (location.pathname.includes(item) && item !== curVersion) {
-                    setCurVersion(item);
-                }
-            });
-        }
-    }, [location.pathname]);
-
-    useEffect(() => {
-        loadIndex(true);
-    }, [curVersion]);
-
     const onClearSearch = useCallback(() => {
         const params = new URLSearchParams(location.search);
         params.delete(SEARCH_PARAM_HIGHLIGHT);
@@ -369,16 +242,12 @@ export default function SearchBar({ handleSearchBarToggle }) {
         setInputValue('');
         search.current?.autocomplete.setVal('');
     }, [location.pathname, location.search, location.hash, history]);
-
     return (
         <div
             className={clsx('navbar__search', styles.searchBarContainer, {
                 [styles.searchIndexLoading]: loading && inputChanged,
                 [styles.focused]: focused,
             })}
-            hidden={hidden}
-            // Manually make the search bar be LTR even if in RTL
-            dir="ltr"
         >
             <input
                 placeholder={translate({
@@ -397,18 +266,15 @@ export default function SearchBar({ handleSearchBarToggle }) {
             />
             <LoadingRing className={styles.searchBarLoadingRing} />
             {searchBarShortcut &&
-                searchBarShortcutHint &&
                 (inputValue !== '' ? (
                     <button className={styles.searchClearButton} onClick={onClearSearch}>
                         ✕
                     </button>
                 ) : (
-                    isBrowser && (
-                        <div className={styles.searchHintContainer}>
-                            <kbd className={styles.searchHint}>{isMac ? '⌘' : 'ctrl'}</kbd>
-                            <kbd className={styles.searchHint}>K</kbd>
-                        </div>
-                    )
+                    <div className={styles.searchHintContainer}>
+                        <kbd className={styles.searchHint}>{isMac ? '⌘' : 'ctrl'}</kbd>
+                        <kbd className={styles.searchHint}>K</kbd>
+                    </div>
                 ))}
         </div>
     );
