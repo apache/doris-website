be0ddcf03f61ccee8d1d5e972903394c81ebe264
liyang <liyang@selectdb.com>
2024-12-11 14:45:09 +0800

    navbar

---
+++ src/theme/Navbar/Content/components/NavbarCommunity.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { useThemeConfig } from '@docusaurus/theme-common';
+import { splitNavbarItems } from '@docusaurus/theme-common/internal';
+import NavbarLogo from '@theme/Navbar/Logo';
+import { type Props as NavbarItemConfig } from '@theme/NavbarItem';
+
+import { getNavItem } from '..';
+import { NavbarItems } from '..';
+import LocaleDropdownNavbarItem from '../../../NavbarItem/LocaleDropdownNavbarItem';
+import styles from '../styles.module.css';
+
+function useNavbarItems() {
+    return useThemeConfig().communityNavbar.items as NavbarItemConfig[];
+}
+
+export const NavbarCommunityLeft = () => {
+    const items = useNavbarItems();
+    const [leftItems] = splitNavbarItems(items);
+    return (
+        <div className={`navbar-left `}>
+            <div className="navbar-logo-wrapper flex items-center">
+                <NavbarLogo />
+            </div>
+            <div className={`${styles.navbarLeftToc}`}>
+                <NavbarItems items={leftItems} />
+            </div>
+        </div>
+    );
+};
+
+export const NavbarCommunityRight = () => {
+    const items = useNavbarItems();
+    const [, rightItems] = splitNavbarItems(items);
+    return (
+        <>
+            {/* {!mobileSidebar.disabled && <NavbarMobileSidebarToggle />} */}
+            <NavbarItems items={rightItems} />
+            {/* <Link className="header-right-button-primary navbar-download-desktop" to="/download">
+            <Translate id="navbar.download">
+                {typeof window !== 'undefined' && location.pathname.includes('zh-CN/docs')
+                    ? '下载'
+                    : 'Download'}
+            </Translate>
+        </Link> */}
+        </>
+    );
+};
+
+export const NavbarCommunityBottom = () => {
+    const items = useNavbarItems();
+    return (
+        <div className="docs-nav-version-locale">
+            <LocaleDropdownNavbarItem mobile={false} {...getNavItem(items, 'localeDropdown')} />
+        </div>
+    );
+};
