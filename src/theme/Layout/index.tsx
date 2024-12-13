fcb4a82eb4f09783e0c80380d9ff02ccd2e7d707
yangon <2689991790@qq.com>
2024-12-13 16:08:02 +0800

    [feat]:searchbar in mobile (#1517)
    ![image](https://github.com/user-attachments/assets/03ceafc2-6e32-4847-897e-11f420a1d3f2)
    
    ---------
    
    Co-authored-by: liyang <liyang@selectdb.com>

--- src/theme/Layout/index.tsx
+++ src/theme/Layout/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, createContext, useContext, useState, useRef } from 'react';
 import clsx from 'clsx';
 import ErrorBoundary from '@docusaurus/ErrorBoundary';
 import { PageMetadata, SkipToContentFallbackId, ThemeClassNames } from '@docusaurus/theme-common';
@@ -9,9 +9,18 @@ import Navbar from '@theme/Navbar';
 import Footer from '@theme/Footer';
 import LayoutProvider from '@theme/Layout/Provider';
 import ErrorPageContent from '@theme/ErrorPageContent';
+import NavbarSearch from '../Navbar/Search';
+import SearchBar from '@theme/SearchBar';
 import type { Props } from '@theme/Layout';
 import styles from './styles.module.css';
 import { useHistory } from '@docusaurus/router';
+
+interface DataType {
+    showSearchPageMobile: boolean;
+    setShowSearchPageMobile: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const DataContext = createContext<DataType>(null);
 
 export default function Layout(props: Props): JSX.Element {
     const {
@@ -23,37 +32,54 @@ export default function Layout(props: Props): JSX.Element {
         description,
     } = props;
     const history = useHistory();
+    const [showSearchPageMobile, setShowSearchPageMobile] = useState(false);
+    const searchPageDom = useRef<HTMLDivElement>(null);
     useKeyboardNavigation();
 
     useEffect(() => {
         if (
             history.location.pathname?.length > 1 &&
             history.location.pathname[history.location.pathname.length - 1] === '/'
-        ){
+        ) {
             const params = location.href.split(history.location.pathname)[1];
             history.replace(history.location.pathname.slice(0, -1) + params);
         }
-           
     }, [history.location]);
 
+    useEffect(() => {
+        if (showSearchPageMobile) {
+            window.scroll(0, 0);
+            document.body.style.overflow = 'hidden';
+            searchPageDom.current.style.height = '100vh';
+            searchPageDom.current.style.display = 'block';
+        } else {
+            searchPageDom.current.style.display = 'none';
+            document.body.style.overflow = 'auto';
+        }
+    }, [showSearchPageMobile]);
+
     return (
-        <LayoutProvider>
-            <PageMetadata title={title} description={description} />
+        <DataContext.Provider value={{ showSearchPageMobile, setShowSearchPageMobile }}>
+            <LayoutProvider>
+                <PageMetadata title={title} description={description} />
 
-            <SkipToContent />
+                <SkipToContent />
+                <AnnouncementBar />
+                <Navbar />
+                <div ref={searchPageDom}>
+                    <NavbarSearch>
+                        <SearchBar />
+                    </NavbarSearch>
+                </div>
+                <div
+                    id={SkipToContentFallbackId}
+                    className={clsx(ThemeClassNames.wrapper.main, styles.mainWrapper, wrapperClassName)}
+                >
+                    <ErrorBoundary fallback={params => <ErrorPageContent {...params} />}>{children}</ErrorBoundary>
+                </div>
 
-            <AnnouncementBar />
-
-            <Navbar />
-
-            <div
-                id={SkipToContentFallbackId}
-                className={clsx(ThemeClassNames.wrapper.main, styles.mainWrapper, wrapperClassName)}
-            >
-                <ErrorBoundary fallback={params => <ErrorPageContent {...params} />}>{children}</ErrorBoundary>
-            </div>
-
-            {!noFooter && <Footer />}
-        </LayoutProvider>
+                {!noFooter && <Footer />}
+            </LayoutProvider>
+        </DataContext.Provider>
     );
 }
