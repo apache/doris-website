ca6c1e9753f79d4e8d2d2211c9529e4525e04f73
yangon <2689991790@qq.com>
2024-12-19 11:22:46 +0800

    Fixed the issue where the announcementbar displayed a white backgroun… (#1544)
    ![image](https://github.com/user-attachments/assets/765b9518-2aa7-4dab-ba07-3f9a30d17ea8)
    
    ---------
    
    Co-authored-by: liyang <liyang@selectdb.com>

--- src/theme/AnnouncementBar/CloseButton/index.tsx
+++ src/theme/AnnouncementBar/CloseButton/index.tsx
@@ -18,7 +18,7 @@ export default function AnnouncementBarCloseButton(
       })}
       {...props}
       className={clsx('clean-btn close', styles.closeButton, props.className)}>
-      <IconClose width={14} height={14} strokeWidth={3.1} />
+      <IconClose width={14} height={14} strokeWidth={3.1} color={'#ffffff'} />
     </button>
   );
 }
