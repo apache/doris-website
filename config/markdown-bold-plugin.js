347902433f955cdb9f518d8e5786b9afcdb1130f
yangon <2689991790@qq.com>
2025-01-06 11:47:53 +0800

    [fix]:Fixed the problem of invalid bolding in markdown documents (#1679)
    Co-authored-by: liyang <liyang@selectdb.com>

---
+++ config/markdown-bold-plugin.js
@@ -0,0 +1,73 @@
+const { visit } = require('unist-util-visit');
+
+function getFirstBoldContent(str, startIdx = 0) {
+    const strArr = str.split("");
+    for (let i = startIdx; i < strArr.length; i++) {
+      if (
+        strArr[i - 1] !== "*" &&
+        strArr[i] === "*" &&
+        strArr[i + 1] === "*" &&
+        strArr[i + 2] !== "*"
+      ) {
+        // start with **
+        let j;
+        for (j = i + 2; j < strArr.length; j++) {
+          if (
+            strArr[j - 1] !== "*" &&
+            strArr[j] === "*" &&
+            strArr[j + 1] === "*" &&
+            strArr[j + 2] !== "*"
+          ) {
+            // end with **
+            return {
+              start: i,
+              end: j,
+            };
+          }
+        }
+      }
+    }
+    return null;
+  }
+
+const plugin = options => {
+    const transformer = async (ast, file) => {
+        visit(ast, 'text', (node, index, parent) => {
+            if (getFirstBoldContent(node.value)) {
+                const { start, end } = getFirstBoldContent(node.value);
+                const value = node.value.slice(start + 2, end);
+                parent.children[index] = {
+                    type: 'strong',
+                    children: [
+                        {
+                            value: value,
+                            type: 'text',
+                        },
+                    ],
+                };
+                const [boldBefore, , boldAfter] = node.value.split('**');
+                let hasBoldBefore = !!boldBefore;
+                if (boldBefore) {
+                    parent.children.splice(index, 0, {
+                        type: 'text',
+                        value: boldBefore,
+                    });
+                }
+                if (boldAfter) {
+                    parent.children.splice(hasBoldBefore ? index + 2 : index + 1, 0, {
+                        type: 'text',
+                        value: boldAfter,
+                    });
+                }
+                console.warn(
+                    `The bold syntax of "${value}" in "${file.path}" is invalid and is being automatically optimized`,
+                );
+            }
+        });
+    };
+    return transformer;
+};
+
+module.exports = {
+    markdownBoldPlugin: plugin,
+};
