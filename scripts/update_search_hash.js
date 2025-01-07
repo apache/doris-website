6db337bceb4bdd547318e3520bee843d5578000e
yangon <2689991790@qq.com>
2025-01-07 15:52:25 +0800

    Feat:automatically update the hash value of searchIndex (#1723)
    Co-authored-by: liyang <liyang@selectdb.com>

---
+++ scripts/update_search_hash.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+
+const { getIndexHash } = require('@easyops-cn/docusaurus-search-local/dist/server/server/utils/getIndexHash.js');
+
+function ensureArray(object, key) {
+    if (!Array.isArray(object[key])) {
+        object[key] = [object[key]];
+    }
+}
+
+const searchConfig = {
+    hashed: true,
+    language: ['en', 'zh'],
+    highlightSearchTermsOnTargetPage: true,
+    docsDir: ['docs', 'versioned_docs', 'i18n'],
+    blogDir: 'blog',
+    // indexPages: true,
+    indexDocs: true,
+    // docsRouteBasePath: '/docs',
+    indexBlog: false,
+    explicitSearchResultPath: true,
+    searchBarShortcut: true,
+    searchBarShortcutHint: true,
+    searchResultLimits: 100,
+};
+ensureArray(searchConfig, 'docsDir');
+ensureArray(searchConfig, 'blogDir');
+searchConfig.docsDir = searchConfig.docsDir.map(dir => path.join(__dirname, `../${dir}`));
+searchConfig.blogDir = searchConfig.blogDir.map(dir => path.join(__dirname, `../${dir}`));
+const searchHash = getIndexHash(searchConfig);
+if (searchHash) {
+    const workerJSPath = path.join(__dirname, '../worker.js');
+    const workerJs = fs.readFileSync(workerJSPath, 'utf-8');
+    const targetRegex = /const searchIndexUrl = "search-index\{dir\}\.json\?_=[^"]+"/;
+    const replacement = `const searchIndexUrl = "search-index{dir}.json?_=${searchHash}"`;
+    const newWorkjs = workerJs.replace(targetRegex, replacement);
+    fs.writeFileSync(workerJSPath, newWorkjs, 'utf-8');
+    console.log('successful,searchHash:',searchHash)
+}else{
+    console.log('searchHash is null');
+}
