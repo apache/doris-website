90036c481d59fb6b7b3dbc0cb9a8b9c6e89230c8
liyang <2689991790@qq.com>
2025-04-01 11:01:29 +0800

    feat:support announcementBar i18n

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -204,12 +204,15 @@ const config = {
             },
             announcementBar: {
                 id: 'join_us',
-                content: `<a href="https://www.velodb.io/events/apache-doris-compute-storage-decoupled-mode-and-velo-db-cloud-demo" target="_blank" style="display:flex; width: 100%; align-items: center; justify-content: center; margin-left: 4px; text-decoration: none;">
+                content: JSON.stringify({
+                    zh: `<a href="https://www.selectdb.com/resources/events/doris-webinar-20250401" target="_blank" style="display:flex; width: 100%; align-items: center; justify-content: center; margin-left: 4px; text-decoration: none;">
                 <img style="width: 19px; height: 19px; margin-right: 3px;" src="/images/nav-star.svg">
                 <span style="color:#52CAA3;font-size:0.875rem;font-weight:700;line-height:1rem; margin-right:0.675rem; text-decoration: none;">NEW</span>
-               <span>Join us for a live webinar to explore Apache Doris' compute-storage decoupled mode</span> 
-               <p style="margin-left:0.675rem;color:#52CAA3;font-size:0.875rem;line-height:1rem;font-weight:700;letter-spacing:0.28px;">Register Now -></p> 
+               <span>4 月 1 日（周二）19:30-21:00 Apache Doris x AI 应用实践直播专场即将来袭！</span> 
+               <p style="margin-left:0.675rem;color:#52CAA3;font-size:0.875rem;line-height:1rem;font-weight:700;letter-spacing:0.28px;">立即报名 -></p> 
                    </a>`,
+                    en: null,
+                }),
                 textColor: '#4C576C',
                 isCloseable: false,
             },
