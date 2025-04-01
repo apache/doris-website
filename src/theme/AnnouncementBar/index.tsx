b9a7342bd3829052851a437a0dc2de75805ee945
yangon <2689991790@qq.com>
2025-04-01 11:35:39 +0800

    feat:support announcementBar i18n (#2247)

--- src/theme/AnnouncementBar/index.tsx
+++ src/theme/AnnouncementBar/index.tsx
@@ -3,25 +3,33 @@ import { useThemeConfig } from '@docusaurus/theme-common';
 import { useAnnouncementBar } from '@docusaurus/theme-common/internal';
 import AnnouncementBarCloseButton from '@theme/AnnouncementBar/CloseButton';
 import AnnouncementBarContent from '@theme/AnnouncementBar/Content';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import { useLocation } from '@docusaurus/router';
 
 import styles from './styles.module.css';
 
-export default function AnnouncementBar(): JSX.Element | null {
+export default function AnnouncementBar(): React.ReactElement | null {
     const { announcementBar } = useThemeConfig();
     const { isActive, close } = useAnnouncementBar();
+    const { i18n } = useDocusaurusContext();
+    const { currentLocale } = i18n;
     const location = useLocation();
-    if (!isActive) {
+    const { backgroundColor, textColor, isCloseable, content } = announcementBar!;
+    
+    if (
+        !isActive ||
+        (currentLocale === 'en' && !JSON.parse(content).en) ||
+        (currentLocale === 'zh-CN' && !JSON.parse(content).zh)
+    ) {
         return null;
     }
-    const { backgroundColor, textColor, isCloseable } = announcementBar!;
     return (
         <div
             className={styles.announcementBar}
             style={
                 location.pathname.startsWith('/zh-CN/docs') || location.pathname.startsWith('/docs')
                     ? { backgroundColor, color: textColor }
-                    : { display:'none' }
+                    : { display: 'none' }
             }
             role="banner"
         >
