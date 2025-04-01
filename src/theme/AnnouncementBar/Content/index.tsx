c82357c61dfad9a9567ae0b66862bc80b09812b1
yangon <2689991790@qq.com>
2025-04-01 11:35:39 +0800

    feat:support announcementBar i18n (#2247)

--- src/theme/AnnouncementBar/Content/index.tsx
+++ src/theme/AnnouncementBar/Content/index.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import clsx from 'clsx';
-import {useThemeConfig} from '@docusaurus/theme-common';
-import type {Props} from '@theme/AnnouncementBar/Content';
+import { useThemeConfig } from '@docusaurus/theme-common';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import type { Props } from '@theme/AnnouncementBar/Content';
 import styles from './styles.module.css';
 
-export default function AnnouncementBarContent(
-  props: Props,
-): JSX.Element | null {
-  const {announcementBar} = useThemeConfig();
-  const {content} = announcementBar!;
-  return (
-    <div
-      {...props}
-      className={clsx(styles.content, props.className)}
-      // Developer provided the HTML, so assume it's safe.
-      // eslint-disable-next-line react/no-danger
-      dangerouslySetInnerHTML={{__html: content}}
-    />
-  );
+export default function AnnouncementBarContent(props: Props): React.ReactElement | null {
+    const { announcementBar } = useThemeConfig();
+    const { i18n } = useDocusaurusContext();
+    const { currentLocale } = i18n;
+    const { content } = announcementBar!;
+
+    return (
+        <div
+            {...props}
+            className={clsx(styles.content, props.className)}
+            // Developer provided the HTML, so assume it's safe.
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+                __html: currentLocale === 'en' ? JSON.parse(content).en : JSON.parse(content).zh,
+            }}
+        />
+    );
 }
