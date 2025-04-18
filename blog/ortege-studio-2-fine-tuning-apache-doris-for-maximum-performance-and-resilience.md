a445a2794d4c44d1b242adc7734a993312bea081
KassieZ <139741991+KassieZ@users.noreply.github.com>
2025-04-18 16:56:44 +0800

    [fix] Fix deadlink of sql data type and update blog (#2302)
    ## Versions 
    
    - [ ] dev
    - [ ] 3.0
    - [ ] 2.1
    - [ ] 2.0
    
    ## Languages
    
    - [ ] Chinese
    - [ ] English
    
    ## Docs Checklist
    
    - [ ] Checked by AI
    - [ ] Test Cases Built

--- blog/ortege-studio-2-fine-tuning-apache-doris-for-maximum-performance-and-resilience.md
+++ blog/ortege-studio-2-fine-tuning-apache-doris-for-maximum-performance-and-resilience.md
@@ -6,8 +6,6 @@
     'date': '2024-11-20',
     'author': 'Justin Trollip',
     'tags': ['Best Practice'],
-    'picked': "true",
-    'order': "4",
     "image": '/images/ortege-2.jpg'
 }
 
--- src/constant/newsletter.data.ts
+++ src/constant/newsletter.data.ts
@@ -1,5 +1,12 @@
 export const NEWSLETTER_DATA = [
     {
+        tags: ['Best Practice'],
+        title: "How Tencent Music saved 80% in costs by migrating from Elasticsearch to Apache Doris",
+        content: `Handle full-text search, audience segmentation, and aggregation analysis directly within Apache Doris and slash their storage costs by 80% while boosting write performance by 4x`,
+        to: '/blog/tencent-music-migrate-elasticsearch-to-doris',
+        image: 'tencent-music-migrate-elasticsearch-to-doris.jpg',
+    },
+    {
         tags: ['Tech Sharing'],
         title: "Slash your cost by 90% with Apache Doris Compute-Storage Decoupled Mode",
         content: `Apache Doris compute-storage decoupled mode achieves 90% cost reduction and provides elasticity and workload isolation, while maintaining high performance in data ingestion and queries.`,
@@ -18,13 +25,6 @@ export const NEWSLETTER_DATA = [
         content: `The comparison in this post will focus on the real-time analytics capabilities of Apache Doris and Elasticsearch from a user-oriented perspective`,
         to: '/blog/why-apache-doris-is-best-alternatives-for-real-time-analytics',
         image: 'es-alternatives/Alternative-to-Elasticsearch.jpg',
-    },
-    {
-        tags: ['Best Practice'],
-        title: "Fine-tuning Apache Doris for maximum performance and resilience: a deep dive into fe.conf",
-        content: `Ortege handles massive volumes of blockchain data to power its analytics platform, Ortege Studio. Apache Doris forms the backbone of its Lakehouse v2, enabling it to process billions of records and deliver real-time insights.`,
-        to: '/blog/ortege-studio-2-fine-tuning-apache-doris-for-maximum-performance-and-resilience',
-        image: 'ortege-2.jpg',
     },
 
 
