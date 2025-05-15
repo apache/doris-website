5821ef0f2a2083ecb0c3d3bfb045785ee92dbee0
kassiez <zhouyuexi@selectdb.com>
2025-05-15 17:26:00 +0800

    update

--- src/components/recent-blogs/recent-blogs.data.ts
+++ src/components/recent-blogs/recent-blogs.data.ts
@@ -1,19 +1,19 @@
 export const RECENT_BLOGS_POSTS = [
     {
-        label: `Apache Doris 2.1.9 Released`,
-        link: 'https://doris.apache.org/blog/release-note-3.0.4',
+        label: `Apache Doris 3.0.5 Released`,
+        link: 'https://doris.apache.org/blog/release-note-3.0.5',
+    },
+    {
+        label: 'How Tencent Music saved 80% in costs by migrating from Elasticsearch to Apache Doris',
+        link: 'https://doris.apache.org/blog/tencent-music-migrate-elasticsearch-to-doris',
+    },
+    {
+        label: 'Slash your cost by 90% with Apache Doris Compute-Storage Decoupled Mode',
+        link: 'https://doris.apache.org/blog/doris-compute-storage-decoupled',
     },
     {
         label: 'Why Apache Doris is a Better Alternative to Elasticsearch for Real-Time Analytics',
         link: 'https://doris.apache.org/blog/why-apache-doris-is-best-alternatives-for-real-time-analytics',
-    },
-    {
-        label: 'Automatic and flexible data sharding: Auto Partition in Apache Doris',
-        link: 'https://doris.apache.org/blog/auto-partition-in-apache-doris',
-    },
-    {
-        label: 'Migrate data lakehouse from BigQuery to Apache Doris, saving $4,500 per month',
-        link: 'https://doris.apache.org/blog/migrate-lakehouse-from-bigquery-to-doris',
     },
 
 
--- src/constant/newsletter.data.ts
+++ src/constant/newsletter.data.ts
@@ -1,5 +1,12 @@
 export const NEWSLETTER_DATA = [
     {
+        tags: ['Best Practice'],
+        title: "How Apache Doris supercharges Cisco WebEx’s data platform",
+        content: `Cisco runs five Doris clusters (dozens of nodes) for WebEx, handling 100,000+ queries per day, and 5TB+ daily real-time data ingestion.`,
+        to: '/blog/doris-supercharges-cisco-webex-data-platform',
+        image: 'blogs/cisco-webex/doris-supercharges-cisco-webex-data-platform.jpg',
+    },
+    {
         tags: ['Release Note'],
         title: "Apache Doris 3.0.5 Released",
         content: `This version features improved Lakehouse, Asynchronous Materialized Views, Semi-structured Data Management and more. `,
@@ -18,14 +25,7 @@ export const NEWSLETTER_DATA = [
         content: `Apache Doris compute-storage decoupled mode achieves 90% cost reduction and provides elasticity and workload isolation, while maintaining high performance in data ingestion and queries.`,
         to: '/blog/doris-compute-storage-decoupled',
         image: 'compute-storage-decoupled-banner.jpg',
-    },
-    {
-        tags: ['Tech Sharing'],
-        title: "Why Apache Doris is a Better Alternative to Elasticsearch for Real-Time Analytics",
-        content: `The comparison in this post will focus on the real-time analytics capabilities of Apache Doris and Elasticsearch from a user-oriented perspective`,
-        to: '/blog/why-apache-doris-is-best-alternatives-for-real-time-analytics',
-        image: 'es-alternatives/Alternative-to-Elasticsearch.jpg',
-    },
+    }
 
 
 ];
