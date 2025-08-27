9faddaad234b234c2c03c2bebc836a9a74a3dac8
KassieZ <139741991+KassieZ@users.noreply.github.com>
2025-08-27 23:38:06 +0800

    Update events banner (#2802)
    ## Versions 
    
    - [ ] dev
    - [ ] 3.0
    - [ ] 2.1
    - [ ] 2.0
    
    ## Languages
    
    - [ ] Chinese
    - [ ] English
    
    ## Docs Checklist
    
    - [ ] Checked by AI
    - [ ] Test Cases Built

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -224,16 +224,16 @@ const config = {
             announcementBar: {
                 id: 'join_us',
                 content: JSON.stringify({
-                    zh: `<a href="https://www.selectdb.com/resources/events/apache-doris-meetup-20250802" target="_blank" style="display:flex; width: 100%; align-items: center; justify-content: center; margin-left: 4px; text-decoration: none;">
+                    zh: `<a href="https://www.selectdb.com/resources/events/doris-webinar-20250828" target="_blank" style="display:flex; width: 100%; align-items: center; justify-content: center; margin-left: 4px; text-decoration: none;">
                 <img style="width: 19px; height: 19px; margin-right: 3px;" src="/images/nav-star.svg">
                 <span style="color:#52CAA3;font-size:0.875rem;font-weight:700;line-height:1rem; margin-right:0.675rem; text-decoration: none;">NEW</span>
-               <span>走进小米 —— Apache Doris 企业行@武汉 Meetup</span> 
+               <span>Apache Doris x Milvus 联合 Webinar：解锁 DB for AI 的无限可能</span> 
                <p style="margin-left:0.675rem;color:#52CAA3;font-size:0.875rem;line-height:1rem;font-weight:700;letter-spacing:0.28px;">查看详情 -></p> 
                    </a>`,
-                    en: `<a href="https://www.velodb.io/events/doris-meetup-250820" target="_blank" style="display:flex; width: 100%; align-items: center; justify-content: center; margin-left: 4px; text-decoration: none;">
+                    en: `<a href="https://www.velodb.io/events/doris-webinar-20250923" target="_blank" style="display:flex; width: 100%; align-items: center; justify-content: center; margin-left: 4px; text-decoration: none;">
                     <img style="width: 19px; height: 19px; margin-right: 3px;" src="/images/nav-star.svg">
                     <span style="color:#52CAA3;font-size:0.875rem;font-weight:700;line-height:1rem; margin-right:0.675rem; text-decoration: none;">NEW EVENTS</span>
-                   <span>Real-Time IoT Analytics — Apache Doris × EMQ Meetup | Aug 20 · San Francisco</span> 
+                   <span>Bridging Data Lake and Database — Apache Doris Webinar | September 23</span> 
                    <p style="margin-left:0.675rem;color:#52CAA3;font-size:0.875rem;line-height:1rem;font-weight:700;letter-spacing:0.28px;">Register Now -></p> 
                        </a>`,
                 }),
