9419138a3e51e073cc1003b5cc86d8c41224d260
liyang <2689991790@qq.com>
2025-10-21 16:32:59 +0800

    Add external link for blog item page

---
+++ src/components/blogs/components/see-more.tsx
@@ -0,0 +1,9 @@
+import { ExternalLinkIcon } from '../../icons/external-link-icon';
+
+export function SeeMore() {
+    return (
+        <span style={{ color: '#444FD9', display: 'inline-flex', alignItems: 'center', gap: '4px' }}>
+            see more <ExternalLinkIcon />
+        </span>
+    );
+}
