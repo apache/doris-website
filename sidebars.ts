23a06b235a38c5860968f82d53424de1e3cf2a97
Hu Shenggang <hushenggang@selectdb.com>
2025-12-02 15:33:38 +0800

    add doc of deduplicate_map

--- sidebars.ts
+++ sidebars.ts
@@ -1589,6 +1589,7 @@ const sidebars: SidebarsConfig = {
                                     type: 'category',
                                     label: 'MAP Functions',
                                     items: [
+                                        'sql-manual/sql-functions/scalar-functions/map-functions/deduplicate-map',
                                         'sql-manual/sql-functions/scalar-functions/map-functions/map',
                                         'sql-manual/sql-functions/scalar-functions/map-functions/map-contains-entry',
                                         'sql-manual/sql-functions/scalar-functions/map-functions/map-contains-key',
