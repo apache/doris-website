0af14548c9966f75f759b2e9241eaa588fe86a25
Vinit Vishwakarma <vvinit594@gmail.com>
2026-01-18 18:42:08 +0530

    Fixed the Button

--- src/components/external-link/external-link.tsx
+++ src/components/external-link/external-link.tsx
@@ -7,13 +7,26 @@ interface ExternalLinkProps extends Props {
     label: string | ReactNode;
     linkIcon?: boolean | ReactNode;
     className?: string;
+    disabled?: boolean;
 }
 export default function ExternalLink(props: ExternalLinkProps) {
-    const { className = 'primary-btn', label, linkIcon = <ExternalLinkIcon />, ...rest } = props;
+    const { className = 'primary-btn', label, linkIcon = <ExternalLinkIcon />, disabled = false, ...rest } = props;
+
+    const disabledProps = disabled
+        ? {
+              'aria-disabled': true,
+              tabIndex: -1,
+              onClick: (event: React.MouseEvent) => event.preventDefault(),
+          }
+        : {};
+
     return (
         <Link
             {...rest}
-            className={`flex group items-center justify-center hover:no-underline external-link ${className}`}
+            {...disabledProps}
+            className={`flex group items-center justify-center hover:no-underline external-link ${className} ${
+                disabled ? 'is-disabled' : ''
+            }`}
         >
             <span className="mr-2">{label}</span>
             <span className="transition-slide">{linkIcon}</span>
