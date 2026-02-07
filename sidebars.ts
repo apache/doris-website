c68731413a1fe843b2d481aefb904afa165d8102
Mingyu Chen (Rayner) <yunyou@selectdb.com>
2026-02-07 12:08:05 +0800

    fix sidebar

--- sidebars.ts
+++ sidebars.ts
@@ -469,7 +469,7 @@ const sidebars: SidebarsConfig = {
                                 'lakehouse/catalogs/delta-lake-catalog',
                                 'lakehouse/catalogs/bigquery-catalog',
                                 'lakehouse/catalogs/kudu-catalog',
-                                'lakehouse/catalogs/kakfa-catalog',
+                                'lakehouse/catalogs/kafka-catalog',
                                 'lakehouse/catalogs/es-catalog',
                                 'lakehouse/catalogs/doris-catalog',
                                 'lakehouse/catalogs/jdbc-catalog-overview',
