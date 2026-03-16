251db1f6ecb0b0b8a253adf89b3c248e45721a26
yangon <2689991790@qq.com>
2026-03-16 19:00:25 +0800

    Remote uesless code

--- build-test.sh
+++
@@ -1,6 +0,0 @@
-export NODE_OPTIONS=--max-old-space-size=8192
-export TEST_ENV_URL=http://localhost:3000
-yarn build
-touch build/.dummy
-cp .asf-site.yaml ./build/.asf-site.yaml
-cp versions.json ./build/
