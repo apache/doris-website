3af2a80258c242abbb417f99d0d1bb556db75aed
SelectDB Pipeline <pipeline@selectdb.com>
2026-04-21 11:24:55 +0800

    add release note and download links for Apache Doris 4.1.0

--- src/constant/download.data.ts
+++ src/constant/download.data.ts
@@ -30,7 +30,7 @@ export enum ToolsEnum {
 
 export const ORIGIN = 'https://download.selectdb.com/';
 export enum VersionEnum {
-    Latest = '4.0.5',
+    Latest = '4.1.0',
     Prev = '3.1.4',
     Earlier = '3.0.8',
 }
@@ -40,6 +40,40 @@ export enum DownloadTypeEnum {
     Source = 'Source',
 }
 export const DORIS_VERSIONS: Option[] = [
+    {
+        label: '4.1.0',
+        value: '4.1.0',
+        majorVersion: '4.1',
+        children: [
+            {
+                label: CPUEnum.X64,
+                value: CPUEnum.X64,
+                gz: `${ORIGIN}apache-doris-4.1.0-bin-x64.tar.gz`,
+                asc: `${ORIGIN}apache-doris-4.1.0-bin-x64.tar.gz.asc`,
+                sha512: `${ORIGIN}apache-doris-4.1.0-bin-x64.tar.gz.sha512`,
+                source: 'https://dist.apache.org/repos/dist/release/doris/4.1/4.1.0/',
+                version: '4.1.0',
+            },
+            {
+                label: CPUEnum.X64NoAvx2,
+                value: CPUEnum.X64NoAvx2,
+                gz: `${ORIGIN}apache-doris-4.1.0-bin-x64-noavx2.tar.gz`,
+                asc: `${ORIGIN}apache-doris-4.1.0-bin-x64-noavx2.tar.gz.asc`,
+                sha512: `${ORIGIN}apache-doris-4.1.0-bin-x64-noavx2.tar.gz.sha512`,
+                source: 'https://dist.apache.org/repos/dist/release/doris/4.1/4.1.0/',
+                version: '4.1.0',
+            },
+            {
+                label: CPUEnum.ARM64,
+                value: CPUEnum.ARM64,
+                gz: `${ORIGIN}apache-doris-4.1.0-bin-arm64.tar.gz`,
+                asc: `${ORIGIN}apache-doris-4.1.0-bin-arm64.tar.gz.asc`,
+                sha512: `${ORIGIN}apache-doris-4.1.0-bin-arm64.tar.gz.sha512`,
+                source: 'https://dist.apache.org/repos/dist/release/doris/4.1/4.1.0/',
+                version: '4.1.0',
+            },
+        ],
+    },
     {
         label: '4.0.5',
         value: '4.0.5',
