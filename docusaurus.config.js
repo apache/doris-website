12b32e836b10e7f6724b30c6903d49b121f2197a
Mingyu Chen (Rayner) <morningman@163.com>
2026-05-23 05:22:13 -0700

    [fix] fix and opt search result and iceberg doc (#3750)
    ## Versions 
    
    - [x] dev
    - [x] 4.x
    - [ ] 3.x
    - [ ] 2.1 or older (not covered by version/language sync gate)
    
    ## Languages
    
    - [x] Chinese
    - [x] English
    - [ ] Japanese candidate translation needed
    
    ## Docs Checklist
    
    - [ ] Checked by AI
    - [ ] Test Cases Built
    - [ ] Updated required version and language counterparts, or explained
    why not
    - [ ] If only one language changed, confirmed whether source/translation
    counterparts need sync

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -383,6 +383,7 @@ const config = {
                 searchResultLimits: 100,
                 searchContextByPaths: ['docs'],
                 useAllContextsWithNoSearchContext: false,
+                ignoreFiles: [/^docs\/(?:[^/]+\/)?key-features\//],
             },
         ],
     ],
